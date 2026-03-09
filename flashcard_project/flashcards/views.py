e2bd4dd6493fc3a9cfa2ba5fa8c0f9a4bc079e74
Anastasia Zelenina <azelenina2003@gmail.com>
2026-03-09 12:10:12 +0300

    add main page

--- flashcard_project/flashcards/views.py
+++ flashcard_project/flashcards/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Deck, Card
+from .forms import DeckForm, CardForm
+
+#список колод - главная страница
+def deck_list(request):
+    decks = Deck.objects.all().order_by('-created_at')  # сортировка: новые сверху
+    return render(request, 'flashcards/deck_list.html', {'decks': decks})
