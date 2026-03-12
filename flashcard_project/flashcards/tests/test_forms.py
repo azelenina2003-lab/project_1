4960a215a394235416486ac5ed85dedcbb4afc00
Anastasia Zelenina <azelenina2003@gmail.com>
2026-03-12 12:28:30 +0300

    add test forms

---
+++ flashcard_project/flashcards/tests/test_forms.py
@@ -0,0 +1,22 @@
+import pytest
+from flashcards.forms import CategoryForm, EntryForm
+from .factories import CategoryFactory
+
+@pytest.mark.django_db
+def test_category_form_valid():
+    form_data = {'name': 'Новая категория', 'description': 'Описание'}
+    form = CategoryForm(data=form_data)
+    assert form.is_valid()
+
+def test_category_form_empty_name():
+    form_data = {'name': '', 'description': 'Описание'}
+    form = CategoryForm(data=form_data)
+    assert not form.is_valid()
+    assert 'name' in form.errors
+
+@pytest.mark.django_db
+def test_entry_form_valid():
+    category = CategoryFactory()
+    form_data = {'term': 'Слово', 'definition': 'Определение'}
+    form = EntryForm(data=form_data)
+    assert form.is_valid()
