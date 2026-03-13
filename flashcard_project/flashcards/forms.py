a3666c74e0fa10e2aaa2852e1d5ef054a4a9d384
Anastasia Zelenina <azelenina2003@gmail.com>
2026-03-13 15:41:46 +0300

    add new form

--- flashcard_project/flashcards/forms.py
+++ flashcard_project/flashcards/forms.py
@@ -1,12 +1,28 @@
 from django import forms
 from .models import Category, Entry
 
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Category, Entry
+
+# форма для колоды
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ['name', 'description']
 
+# форма для карточки
+
 class EntryForm(forms.ModelForm):
     class Meta:
         model = Entry
         fields = ['term', 'definition']
+
+# форма для регистрации
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Обязательное поле.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
